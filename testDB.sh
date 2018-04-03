47a3de0fe529137a3a69f485f97b87bf2df1a81e
Raymond Lochner <ray_lochner@yahoo.de>
2018-04-03 23:30:41 +0200

    Deliberately adding incorrect deletion script to see if travisci will see it as failed

--- testDB.sh
+++ testDB.sh
@@ -12,6 +12,7 @@ function executeSQL() {
 }
 
 executeSQL scripts/db_table_drop_constraints.sql
+executeSQL scripts/db_table_drop_constraints.sql
 executeSQL scripts/db_table_drop.sql
 executeSQL scripts/db_table_creation.sql
 executeSQL scripts/db_table_add_constraints.sql
