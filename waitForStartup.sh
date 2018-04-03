f05442c1d663047af9295524df0de983d4c4f16a
Raymond Lochner <ray_lochner@yahoo.de>
2018-04-03 20:24:23 +0200

    Trying to check for startup of database

---
+++ waitForStartup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+echo "Starting database container" 
+
+# Max query attempts before consider setup failed
+MAX_TRIES=3
+
+# Returns a true-like value if and only if a query
+# with the expected db and user succeeds
+function dbIsReady() {
+  PGPASSWORD=password \
+  psql -h localhost:5432 -U postgres \
+       -d postgres -c "select 1" \
+  > /dev/null 2>&1
+}
+
+echo "Wait for PostgreSQL to become available..."
+sleep 5
+until dbIsReady || [ $MAX_TRIES -eq 0 ]; do
+  echo "Waiting for PostgreSQL server, $((MAX_TRIES--)) remaining attempts..."
+  sleep 5
+done
+
+if [ $MAX_TRIES -eq 0 ]
+then
+  echo "Error: PostgreSQL not responding, cancelling server set up"
+  exit 1
+fi
